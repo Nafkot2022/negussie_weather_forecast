29a402b921633ac750bd01ce10259775cc551b70
Naf <nnegussie7@gmail.com>
2022-10-12 22:08:18 -0400

    display data function

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,46 @@
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={c2a19b7d3378306b6ba043066d90fe52}
 
+//search button
+const searchBtn= document.getElementById('submitCityBtn');
+const cityInputEl=document.getElementById('cityInput');
 
-let textInput = "chicago";
-
-fetch('https://api.openweathermap.org/geo/1.0/direct?q=chicago&limit=5&appid=c2a19b7d3378306b6ba043066d90fe52')
+function searchCity(){
+console.log('hello');
+let textInput =cityInputEl.value;
+console.log(cityInputEl.value);
+fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${textInput}&limit=5&appid=c2a19b7d3378306b6ba043066d90fe52`)
 		.then((response) => response.json())
-		.then((data) => console.log(data));
+		.then((data) =>{
+			//console.log(data);
+			data[0];
+			//console.log(data[0]);
+			//console.log(data[0].lat)
+			//console.log(data[0].lon)
+			displayData(data[0].lat,data[0].lon);
+		});
+};
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=c2a19b7d3378306b6ba043066d90fe52')
+function displayData(lat,lon){
+
+	fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=c2a19b7d3378306b6ba043066d90fe52`)
 		.then((response) => response.json())
-		.then((data) => console.log(data));
+		.then((data) =>{
+		let current = data.current;
+		let temp = current.temp;
+		let humidity = current.humidity;
+		let date = formatUnixTimeStamp(current.dt);
+		let windSpeed = current.wind_speed;
+
+		console.log(temp,humidity,date,windSpeed);
+			
+		});
+};
+//current date conversion 
+function formatUnixTimeStamp(unixTime) {
+  const date = new Date(unixTime * 1000);
+  return date.toLocaleDateString('en-US');
+}
+
+
+
+searchBtn.onclick = searchCity;
